70e8da64d24afd40ab615d84cf9913bb477d736c
Vikash Kumar <96245971+Vikash0122@users.noreply.github.com>
2024-08-08 20:33:01 +0530

    Project-Employees-I.sql

---
+++ Project-Employees-I.sql
@@ -0,0 +1,7 @@
+SELECT p.project_id, 
+IFNULL(ROUND(SUM(e.experience_years)/COUNT(p.project_id),2),0)
+AS average_years
+FROM Project p
+LEFT JOIN Employee e
+ON p.employee_id = e.employee_id
+GROUP BY p.project_id;
